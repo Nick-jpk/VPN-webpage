d1b117557b5a1da329f23b1589876d3bd21aca1d
Nick-jpk <nickheavens52@gmail.com>
2025-09-18 02:54:46 +0300

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import VPNPage from './pages/VPNPage';
-import AdminLogin from './admin/AdminLogin';
-import Dashboard from './admin/Dashboard';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vpn" element={<VPNPage />} />
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Victonnel VPN Dashboard</h1>
+    </div>
   );
 }
 
